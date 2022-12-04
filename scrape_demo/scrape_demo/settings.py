706da662ccde3723e311a5483e1bf8158a1b83ea
yuoku <appledev.yuoku@gmail.com>
2022-12-04 21:22:53 +0900

    Qiitaのitems APIをリクエストするようSpiderファイルを更新

--- scrape_demo/scrape_demo/settings.py
+++ scrape_demo/scrape_demo/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrape_demo.spiders'
 #USER_AGENT = 'scrape_demo (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- scrape_demo/scrape_demo/spiders/page_spider.py
+++ scrape_demo/scrape_demo/spiders/page_spider.py
@@ -1,10 +1,25 @@
+import datetime
 import scrapy
+import json
 
 
 class PageSpiderSpider(scrapy.Spider):
     name = 'page_spider'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    allowed_domains = ['qiita.com']
+
+    def start_requests(self):
+        base_url = 'https://qiita.com/api/v2/items?page=1&per_page=10&query=user%3Ay-okudera'
+        request_headers  = {
+            'Content-Type': 'application/json',
+        }
+        yield scrapy.Request(url=base_url, headers=request_headers, callback=self.parse)
 
     def parse(self, response):
-        pass
+        json_response = json.loads(response.text)
+        self.write_json(json_response)
+
+    def write_json(self, json_response):
+        now = datetime.datetime.now()
+        filename = './output/log_' + now.strftime('%Y%m%d_%H%M%S') + '.json'
+        with open(filename, 'w') as f:
+            json.dump(json_response, f, indent=4)
