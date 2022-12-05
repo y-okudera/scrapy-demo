73f2fe575313ee254af3eca66202301928b6a9aa
okudera <appledev.yuoku@gmail.com>
2022-12-05 18:05:18 +0900

    Update qiita_items_spider.py

--- scrape_demo/scrape_demo/spiders/qiita_items_spider.py
+++ scrape_demo/scrape_demo/spiders/qiita_items_spider.py
@@ -8,8 +8,8 @@ class QiitaItemsSpider(scrapy.Spider):
     allowed_domains = ['qiita.com']
 
     def start_requests(self):
-        base_url = 'https://qiita.com/api/v2/items?page=1&per_page=10&query=user%3Ay-okudera'
-        request_headers  = {
+        base_url = 'https://qiita.com/api/v2/items?page=1&per_page=10&query=Python'
+        request_headers = {
             'Content-Type': 'application/json',
         }
         yield scrapy.Request(url=base_url, headers=request_headers, callback=self.parse)
